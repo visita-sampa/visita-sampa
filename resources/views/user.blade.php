ee43041b65c7e07f648fea52c0a8040ec88207cb
Igor Mozetic <igormozetic04@gmail.com>
2022-06-19 19:16:26 -0300

    deploy segundo bimestre

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,7 +7,6 @@
     <title>{{ __('Meu Perfil') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="/assets/css/style.css" rel="stylesheet">
-    <link href="/assets/css/quiz.css" rel="stylesheet">
     <link href="/assets/css/user.css" rel="stylesheet">
     <link href="/assets/icon/style.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
