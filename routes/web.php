a80b62db769ec0b906c6908d1c4bf52712e16f41
Igor Mozetic <igormozetic04@gmail.com>
2022-08-21 19:57:31 -0300

    deploy terceiro bimestre

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ use App\Http\Controllers\AdminEventsController;
 */
 
 Route::redirect('/', '/pt/home');
+Route::redirect('/pt', '/pt/home');
+Route::redirect('/en', '/en/home');
 // Route::redirect('/', '/pt');
 
 
@@ -34,7 +36,7 @@ Route::group(['prefix' => '{language?}'], function () {
     Route::get('/', function () {
         return view('welcome');
     });
-    
+
     Route::get('/quiz', [QuizController::class, 'index'])->name('quiz');
     Route::get('/event', [EventController::class, 'index'])->name('event');
     Route::get('/feed', [FeedController::class, 'index'])->name('feed');
@@ -46,21 +48,21 @@ Route::group(['prefix' => '{language?}'], function () {
 
 
     Route::get('/deleteEvent', [EventController::class, 'deleteEventByExpirationDate'])->name('delete.event');
-    
+
 
     Route::get('/touristSpot/{id?}', [TouristSpotController::class, 'show'])->name('touristSpot.show');
-    
+
     Route::post('/send', [QuizController::class, 'store'])->name('quiz.store');
-    
+
     Route::get('/byCookie', [RoadMapController::class, 'calculatePersonalityByCookies'])->name('byCookie');
-    
+
     // user registration routes
     Route::get('/register', [UserController::class, 'register'])->name('register');
     Route::post('/register', [UserController::class, 'store'])->name('user.store');
-    
+
     Route::get('/emailConfirmation/{key?}', [UserController::class, 'emailConfirmation'])->name('email.confirmation');
     // Route::get('/emailconfirmationfail', [UserController::class, 'emailConfirmationFail'])->name('email.confirmation.fail');
-    
+
     // user authentication routes
     Route::get('/login', [LoginController::class, 'login'])->name('login');
     Route::post('/login', [LoginController::class, 'validateLogin'])->name('validate.login');
@@ -69,7 +71,7 @@ Route::group(['prefix' => '{language?}'], function () {
     Route::post('/passwordRequest', [LoginController::class, 'passwordRequest'])->name('password.request');
     Route::get('/resetPassword/{key?}', [LoginController::class, 'resetPassword'])->name('reset.password');
     Route::post('/updatePassword', [LoginController::class, 'updatePassword'])->name('update.password');
-    
+
     // Route::post('/addEvent', [EventController::class, 'store'])->name('event.store');
     // Route::post('/addTouristSpot', [TouristSpotController::class, 'store'])->name('touristSpot.store');
 
@@ -77,7 +79,7 @@ Route::group(['prefix' => '{language?}'], function () {
     Route::get('/email/verify', [VerificationController::class, 'show'])->name('verification.notice');
     Route::get('/email/verify/{id}', [VerificationController::class, 'verify'])->name('verification.verify');
     Route::get('/email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
-    
+
     Route::group(['middleware' => ['auth']], function () {
         // upload publications routes
         Route::post('/addPost', [PublicationController::class, 'store'])->name('publication.store');
