1ccfe07dfcf6b09399705087d1bb5e4e921b467d
Igor Mozetic <igormozetic04@gmail.com>
2022-09-05 04:46:00 -0300

    deploy terceiro bimestre

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -64,7 +64,7 @@
       </div>
       <div class="toast-body">
         @if(session('msgUpdatePasswordSuccess'))
-        {{ __(session('msgUpdatePasswordSuccess') }}
+        {{ __(session('msgUpdatePasswordSuccess')) }}
 
         @elseif(session('msgSignupCompleted'))
         {{ __(session('msgSignupCompleted')) }}
@@ -96,7 +96,7 @@
     var tooltipTriggerList = [].slice.call(
       document.querySelectorAll('[data-bs-toggle="tooltip"]')
     );
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
       return new bootstrap.Tooltip(tooltipTriggerEl);
     });
 
