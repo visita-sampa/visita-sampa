2a840003ae969a40bf4ae9e78172fe0880dea7ad
Igor Mozetic <igormozetic04@gmail.com>
2022-11-05 02:39:16 -0300

    deploy quarto bimestre

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,7 @@
   script-src 'self' 'unsafe-eval' https://ajax.googleapis.com/ajax/
      https://cdnjs.cloudflare.com/ajax/ https://activity-project-unit.herokuapp.com ;
   font-src https://fonts.gstatic.com data: ;">
+  <meta name="referrer" content="origin" />
   <title>Visita Sampa</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
   <link href="/assets/css/style.css" rel="stylesheet" />
