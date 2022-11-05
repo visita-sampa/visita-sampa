d409043d16764ed64beac71ea8304ca7625a6b19
Igor Mozetic <igormozetic04@gmail.com>
2022-11-05 02:45:10 -0300

    deploy quarto bimestre

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,6 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="Content-Security-Policy" content="default-src 'self' data: gap: ws: ;
-  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com
-     https://cdn.jsdelivr.net/npm/ https://cdnjs.cloudflare.com/ajax/ ;
-  script-src 'self' 'unsafe-eval' https://ajax.googleapis.com/ajax/
-     https://cdnjs.cloudflare.com/ajax/ https://activity-project-unit.herokuapp.com ;
-  font-src https://fonts.gstatic.com data: ;">
   <meta name="referrer" content="origin" />
   <title>Visita Sampa</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
