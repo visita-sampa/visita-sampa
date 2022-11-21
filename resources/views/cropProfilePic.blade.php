031b03a5bb91eba48b383a2b13517b8669c08631
Igor Mozetic <igormozetic04@gmail.com>
2022-11-21 20:46:30 -0300

    deploy quarto bimestre

--- resources/views/cropProfilePic.blade.php
+++ resources/views/cropProfilePic.blade.php
@@ -14,7 +14,7 @@
         <div class="img-container">
           <div class="row">
             <div class="col-md-11">
-              <img id="image" src="https://avatars0.githubusercontent.com/u/3456749" class="d-block mw-100 img-crop">
+              <img id="image" src="https://avatars0.githubusercontent.com/u/3456749" class="d-block mw-100 img-crop" referrerpolicy="no-referrer">
             </div>
           </div>
         </div>
@@ -29,51 +29,51 @@
 
 <button type="button" class="btn btn-primary d-none" id="btnToastEditProfile">Show live toast</button>
 
-  <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
-    <div id="liveToastEditProfile" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-      <div class="toast-header">
-        @if(session('msgUpdateProfileSuccess'))
-        <strong class="me-auto text-success">
-          <i class="icon-check"></i>
-          {{ __('Sucesso') }}
-        </strong>
-        @elseif(session('msgUpdateProfileFail'))
-        <strong class="me-auto text-danger">
-          <i class="icon-x"></i>
-          {{ __('Falha') }}
-        </strong>
-        @endif
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-      </div>
-      <div class="toast-body">
-        @if(session('msgUpdateProfileSuccess'))
-        {{ session('msgUpdateProfileSuccess') }}
+<div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+  <div id="liveToastEditProfile" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      @if(session('msgUpdateProfileSuccess'))
+      <strong class="me-auto text-success">
+        <i class="icon-check"></i>
+        {{ __('Sucesso') }}
+      </strong>
+      @elseif(session('msgUpdateProfileFail'))
+      <strong class="me-auto text-danger">
+        <i class="icon-x"></i>
+        {{ __('Falha') }}
+      </strong>
+      @endif
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+      @if(session('msgUpdateProfileSuccess'))
+      {{ session('msgUpdateProfileSuccess') }}
 
-        @elseif(session('msgUpdateProfileFail'))
-        {{ session('msgUpdateProfileFail') }}
+      @elseif(session('msgUpdateProfileFail'))
+      {{ session('msgUpdateProfileFail') }}
 
-        @endif
-      </div>
+      @endif
     </div>
   </div>
-  
-  <script src="/assets/js/cropimage.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.js"></script>
+</div>
+
+<script src="/assets/js/cropimage.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.js"></script>
 
 <script>
   var btnToastEditProfile = document.getElementById("btnToastEditProfile");
   var liveToastEditProfile = document.getElementById("liveToastEditProfile");
   if (btnToastEditProfile) {
-    btnToastEditProfile.addEventListener("click", function () {
-          var toast = new bootstrap.Toast(liveToastEditProfile);
-  
-          toast.show();
-      });
+    btnToastEditProfile.addEventListener("click", function() {
+      var toast = new bootstrap.Toast(liveToastEditProfile);
+
+      toast.show();
+    });
   }
 
   @if(session('msgUpdateProfileSuccess') || session('msgUpdateProfileFail'))
-    $(document).ready(function() {
-      $("#btnToastEditProfile").click();
-    });
+  $(document).ready(function() {
+    $("#btnToastEditProfile").click();
+  });
   @endif
 </script>
